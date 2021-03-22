a5b5a9ad622d95f1a36d942c48ba9abbab01c59e
Adam Schwartz <adamberard24@gmail.com>
2021-03-21 20:36:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   let changes = document.getElementById('text')
+   changes.textContent = "This is really cool!"
+})
